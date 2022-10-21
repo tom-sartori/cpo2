5cd3cf16a3ebf5dfff7280cf75dec82b62386ae6
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2022-10-21 17:32:44 +0200

    Select data.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,21 @@
+import java.sql.*;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Hello world!");
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5434/app_db",
+                "postgres", "postgres");
+
+        System.out.println(connection.isValid(1));
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM users;");
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("first_name"));
+        }
     }
 }
